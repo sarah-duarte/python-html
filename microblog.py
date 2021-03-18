6950b2a5b9627db0001df598eb7df45960212a23
Sarah Duarte <sarahf.fduarte@gmail.com>
2021-03-18 18:45:35 +0000

    Hello-world com flask

--- microblog.py
+++ microblog.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app = Flask("microblog")
-
-#decorator - operador do python que fica em cima da função e ele faz alguma coisa com essa função. 
-@app.route("/")
-def index():
-    return "Hello World"
-
-app.run()
+from app import app
