5f9f3f27501d57245f483b1db7d1ca082e849dac
Sarah Duarte <sarahf.fduarte@gmail.com>
2021-03-18 21:55:36 +0000

    Com login

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 
 @app.route('/')
@@ -8,6 +8,12 @@ def index():
     posts = [
         { 'author': {'username': 'John'}, 'body': 'Beautiful day in Portland!' },
         { 'author': {'username': 'Susan'}, 'body': 'The Avengers movie was so cool!' },
-        { 'author': {'username': 'Sarah'}, 'body': 'The Avengers movie was so cool!' }
+        { 'author': {'username': 'Miguel'}, 'body': 'The Avengers movie was so cool!' }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        print(request.values.get("user"), request.values.get("pass"), request.values.get("remember"))
+    return render_template("login.html", title="Login")
