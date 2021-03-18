9ecb1c81556f925a854f44374a4e663e49ba1f9f
Sarah Duarte <sarahf.fduarte@gmail.com>
2021-03-18 23:06:29 +0000

    parte 5

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
-from app import routes
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "banana"
+db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
+
+from app import routes, models
+db.create_all()
---
+++ app/models.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from app import db, login
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password = db.Column(db.String(64))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    @login.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
